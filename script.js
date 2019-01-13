d5219462f0552c3a6dd04afd9a8feeae09ae8487
sloansobie <sloan.sobie@gmail.com>
2019-01-13 14:04:10 -0500

    added button and tried having hidden message and countdown but didn't work

---
+++ script.js
@@ -0,0 +1,9 @@
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display= 'block';
+}
+
+	function countDown() {
+		var current Val = document.getElementById("countDownButton").innerHTML;
+		var newVal = currenVal + 1;
+		document.getElementById("countDownButton").innerHTML = newVal
+	}
